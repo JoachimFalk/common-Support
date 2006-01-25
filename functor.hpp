2ff1598c5dd0efc16addfeba7c0cee27176d3887
Joachim Falk <falk@cs.fau.de>
2006-01-25 13:52:43 +0000

    * Cleaned up macros for Functor and ConstFunctor definition * Now support up to 4 function parameters
    git-archimport-id: joachim.falk@gmx.de--2005-common-stuff/HscdSupport--mainline--0.2--patch-13

--- functor.hpp
+++ functor.hpp
@@ -95,53 +95,96 @@ struct ParamAccumulator<A, F, MissingNode<void>, PL>
 //
 // FUNCTOR-macro: Creates Function-Objects
 // 
-#define FUNCTOR(NAME, TLIST, PLIST, CONST, PCALL, MISSING)			\
+#define CONSTRUCT(NAME, TLIST, PLIST, CONST, PCALL, MISSING)			\
 template<class R, class T TLIST>						\
-struct NAME<R, R (T::*)PLIST CONST> {						\
-	typedef NAME<R, R (T::*)PLIST CONST>			    this_type;	\
+struct NAME<R, R (T::*)(PLIST) CONST> {						\
+	typedef NAME<R, R (T::*)(PLIST) CONST>			    this_type;	\
 	typedef MISSING						    MissingList;\
 	typedef R						    return_type;\
 	typedef typename MissingToParamList<MissingList>::ParamList ParamList;	\
 										\
 	CONST T *obj;								\
-	R (T::*func)PLIST CONST;						\
+	R (T::*func)(PLIST) CONST;						\
 	const char *name;							\
 										\
 	template<class X>							\
-	NAME(CONST X *_obj, R (T::*_func)PLIST CONST, const char *_name)	\
+	NAME(CONST X *_obj, R (T::*_func)(PLIST) CONST, const char *_name)	\
 	  : obj(dynamic_cast<CONST T *>(_obj)), func(_func), name(_name)	\
 	  { assert(obj != 0 && func != 0); }					\
 										\
 	R call(const ParamList &pl) const {					\
-		return (obj->*func)PCALL;					\
+		return (obj->*func)(PCALL);					\
 	}									\
 };
 
-#define _CT_ , typename
-#define _MISSING_0_ MissingNode<void>
-#define _MISSING_1_ MissingNode<P1, MissingNode<void> >
-#define _MISSING_2_ MissingNode<P1, MissingNode<P2, MissingNode<void> > >
+#define MISSING_0 MissingNode<void>
+#define MISSING_1 MissingNode<P1 , MISSING_0 >
+#define MISSING_2 MissingNode<P2 , MISSING_1 >
+#define MISSING_3 MissingNode<P3 , MISSING_2 >
+#define MISSING_4 MissingNode<P4 , MISSING_3 >
+
+#define TEMPLATELIST_0 
+#define TEMPLATELIST_1 , typename P1 TEMPLATELIST_0
+#define TEMPLATELIST_2 , typename P2 TEMPLATELIST_1
+#define TEMPLATELIST_3 , typename P3 TEMPLATELIST_2
+#define TEMPLATELIST_4 , typename P4 TEMPLATELIST_3
+
+#define PARAMLIST_0
+#define PARAMLIST_1 P1
+#define PARAMLIST_2 P2 , PARAMLIST_1
+#define PARAMLIST_3 P3 , PARAMLIST_2
+#define PARAMLIST_4 P4 , PARAMLIST_3
+
+#define PARAMCALL_0
+#define PARAMCALL_1 pl.p
+#define PARAMCALL_2 pl.pn.p , PARAMCALL_1
+#define PARAMCALL_3 pl.pn.pn.p , PARAMCALL_2
+#define PARAMCALL_4 pl.pn.pn.pn.p , PARAMCALL_3
 
 template<class R, class F>
 struct Functor;
 
-FUNCTOR(     Functor,                ,       (),      ,              (), _MISSING_0_)
-FUNCTOR(     Functor,         _CT_ P1,     (P1),      ,          (pl.p), _MISSING_1_)
-FUNCTOR(     Functor, _CT_ P1 _CT_ P2, (P1, P2),      , (pl.pn.p, pl.p), _MISSING_2_)	
-
+CONSTRUCT(     Functor, TEMPLATELIST_0, PARAMLIST_0,      , PARAMCALL_0, MISSING_0)
+CONSTRUCT(     Functor, TEMPLATELIST_1, PARAMLIST_1,      , PARAMCALL_1, MISSING_1)
+CONSTRUCT(     Functor, TEMPLATELIST_2, PARAMLIST_2,      , PARAMCALL_2, MISSING_2)
+CONSTRUCT(     Functor, TEMPLATELIST_3, PARAMLIST_3,      , PARAMCALL_3, MISSING_3)
+CONSTRUCT(     Functor, TEMPLATELIST_4, PARAMLIST_4,      , PARAMCALL_4, MISSING_4)
+  
 template<class R, class F>
 struct ConstFunctor;
 
-FUNCTOR(ConstFunctor,                ,       (), const,              (), _MISSING_0_)
-FUNCTOR(ConstFunctor,         _CT_ P1,     (P1), const,          (pl.p), _MISSING_1_)
-FUNCTOR(ConstFunctor, _CT_ P1 _CT_ P2, (P1, P2), const, (pl.pn.p, pl.p), _MISSING_2_)
+CONSTRUCT(ConstFunctor, TEMPLATELIST_0, PARAMLIST_0, const, PARAMCALL_0, MISSING_0)
+CONSTRUCT(ConstFunctor, TEMPLATELIST_1, PARAMLIST_1, const, PARAMCALL_1, MISSING_1)
+CONSTRUCT(ConstFunctor, TEMPLATELIST_2, PARAMLIST_2, const, PARAMCALL_2, MISSING_2)
+CONSTRUCT(ConstFunctor, TEMPLATELIST_3, PARAMLIST_3, const, PARAMCALL_3, MISSING_3)
+CONSTRUCT(ConstFunctor, TEMPLATELIST_4, PARAMLIST_4, const, PARAMCALL_4, MISSING_4)
+  
+#undef CONSTRUCT
 
-#undef _MISSING_2_
-#undef _MISSING_1_
-#undef _MISSING_0_
-#undef _CT_
-#undef FUNCTOR
+#undef MISSING_0
+#undef MISSING_1
+#undef MISSING_2
+#undef MISSING_3
+#undef MISSING_4
 
+#undef TEMPLATELIST_0
+#undef TEMPLATELIST_1
+#undef TEMPLATELIST_2
+#undef TEMPLATELIST_3
+#undef TEMPLATELIST_4
+
+#undef PARAMLIST_0
+#undef PARAMLIST_1
+#undef PARAMLIST_2
+#undef PARAMLIST_3
+#undef PARAMLIST_4
+
+#undef PARAMCALL_0
+#undef PARAMCALL_1
+#undef PARAMCALL_2
+#undef PARAMCALL_3
+#undef PARAMCALL_4
+  
 } // namespace CoSupport
 
 #endif // _FUNCTOR_HPP_
