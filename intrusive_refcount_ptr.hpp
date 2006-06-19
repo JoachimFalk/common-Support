fc6b82a3c55324695fe8ba57be52153f7c6b9d86
Joachim Falk <falk@cs.fau.de>
2006-06-19 08:52:27 +0000

    * Merge from joachim.falk@gmx.de--2004-jf-stuff/Support--mainline--0.1 - DECL_INTRUSIVE_COPYONWRITE_PTR and IMPL_INTRUSIVE_COPYONWRITE_PTR hardcode JFLibs namespace to make macros useable from other namespaces too. - Use new makefile var auxdir generated in ACJF_DONE - BUGFIX for intrusive_copyonwrite_ptr fixed inverted logic for operator bool !!! - Rename refonly_count => RefCount - Use ACJF_VAR_SUBPROJECT_DIR for configure.in.frag - Added intrusive_refcount_ptr which defines DECL_INTRUSIVE_REFCOUNT_PTR(T, PST) IMPL_INTRUSIVE_REFCOUNT_PTR(T)
    Patches applied:
    
     * joachim.falk@gmx.de--2004-jf-stuff/Support--mainline--0.1--patch-17
       
     * Merge from joachim.falk@gmx.de--2005-common-stuff/HscdSupport--mainline--0.2 branch
     - gcc-3.4 compile fixes
    
     * joachim.falk@gmx.de--2004-jf-stuff/Support--mainline--0.1--patch-18
       
     * Merge from HscdSupport--mainline--0.2 Branch
     - Added functor.hpp used for variable (0-4) parameter methods
    
     * joachim.falk@gmx.de--2004-jf-stuff/Support--mainline--0.1--patch-20
       
     * DECL_INTRUSIVE_COPYONWRITE_PTR and IMPL_INTRUSIVE_COPYONWRITE_PTR
       hardcode JFLibs namespace to make macros useable from other namespaces
       too
    
     * joachim.falk@gmx.de--2004-jf-stuff/Support--mainline--0.1--patch-21
       
     * Merge from HscdSupport--mainline--0.2 branch
     - Don't use const references to assemble ParamList in functor.hpp
       therefore allowing to use reference arguments in guards and actions, e.g,
       dosomething( smoc_port_in<double> &in ) { ... }
       Should only be used for smoc port references
    
     * joachim.falk@gmx.de--2004-jf-stuff/Support--mainline--0.1--patch-22
       
     * Merge from HscdSupport--mainline--0.2 branch
     - added visitor functionality for oneof (-> applyVisitor)
    
     * joachim.falk@gmx.de--2004-jf-stuff/Support--mainline--0.1--patch-23
       
     * Merge from HscdSupport--mainline--0.2 branch
     - added non-const visitors
    
     * joachim.falk@gmx.de--2004-jf-stuff/Support--mainline--0.1--patch-24
       
     * Merge from HscdSupport--mainline--0.2 branch
     - g++-3.4 compile fixes
    
     * joachim.falk@gmx.de--2004-jf-stuff/Support--mainline--0.1--patch-25
       
     * Use new makefile var auxdir generated in ACJF_DONE
       needs joachim.falk@gmx.de--2004-jf-stuff/BuildSystem--mainline--0.1--patch-12
    
     * joachim.falk@gmx.de--2004-jf-stuff/Support--mainline--0.1--patch-26
       
     * BUGFIX for intrusive_copyonwrite_ptr fixed inverted logic for operator bool !!!
    
     * joachim.falk@gmx.de--2004-jf-stuff/Support--mainline--0.1--patch-27
       
     * Rename refonly_count => RefCount
    
     * joachim.falk@gmx.de--2004-jf-stuff/Support--mainline--0.1--patch-29
       
     * Use ACJF_VAR_SUBPROJECT_DIR for configure.in.frag
    
     * joachim.falk@gmx.de--2004-jf-stuff/Support--mainline--0.1--patch-30
       
     * Added intrusive_refcount_ptr which defines
     - DECL_INTRUSIVE_REFCOUNT_PTR(T, PST)
     - IMPL_INTRUSIVE_REFCOUNT_PTR(T)
    
    
    git-archimport-id: joachim.falk@gmx.de--2005-common-stuff/HscdSupport--mainline--0.2--patch-23

---
+++ intrusive_refcount_ptr.hpp
@@ -0,0 +1,42 @@
+/* vim: set sw=2 ts=8: */
+/*
+ * Copyright (C) Joachim Falk <joachim.falk@gmx.de> $Date: 2001/08/28 12:01:41 $
+ *
+ * refcount.h is part of the jf-libs distribution of Joachim Falk;
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation;
+ * either version 2 of the License, or (at your option) any later version.
+ *
+ * The jf-libs distributio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef _INCLUDED_JFLIBS_INTRUSIVE_REFCOUNT_PTR_HPP
+#define _INCLUDED_JFLIBS_INTRUSIVE_REFCOUNT_PTR_HPP
+
+#include "refcount.hpp"
+
+#include <boost/intrusive_ptr.hpp>
+
+#define DECL_INTRUSIVE_REFCOUNT_PTR(T,PST)	                \
+  typedef ::boost::intrusive_ptr<T> PST;                        \
+  void intrusive_ptr_add_ref(T *);		                \
+  void intrusive_ptr_release(T *);
+
+#define IMPL_INTRUSIVE_REFCOUNT_PTR(T)		                \
+  void intrusive_ptr_add_ref(T *p) {		                \
+    p->add_ref();				                \
+  }						                \
+  void intrusive_ptr_release(T *p) {		                \
+    if ( p->del_ref() )				                \
+      delete p;					                \
+  }
+
+#endif // _INCLUDED_JFLIBS_INTRUSIVE_REFCOUNT_PTR_HPP
