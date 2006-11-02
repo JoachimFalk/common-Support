fb0259773dce6817af95a10c18d8f29fb73d1701
Joachim Falk <joachim.falk@gmx.de>
2006-11-02 09:59:36 +0000

    * Merge from HscdSupport--mainline--0.5 branch fixing the patches HscdSupport--mainline--0.3--patch-23 and patch-24 - Fixed debug output of oneof class - Fixed infinited stack recursion in oneof class
    Keywords: 
    
    
    
    Patches applied:
    
     * joachim.falk@gmx.de--2006-common-stuff/HscdSupport--mainline--0.5--patch-5
       
     * Fixed debug output of oneof class
    
     * joachim.falk@gmx.de--2006-common-stuff/HscdSupport--mainline--0.5--patch-6
       
     * Fixed infinited stack recursion in oneof class
    
    
    git-archimport-id: joachim.falk@gmx.de--2006-common-stuff/HscdSupport--mainline--0.3--patch-25

--- oneof.hpp
+++ oneof.hpp
@@ -184,7 +184,7 @@ const oneof_typeid oneofTypeid<const oneof<T1,T2,T3,T4,T5>,NILTYPE>::type = 0;
 
 template <typename T, typename T1, typename T2, typename T3, typename T4, typename T5>
 static inline
-bool isType( const oneof<T1,T2,T3,T4,T5> of )
+bool isType( const oneof<T1,T2,T3,T4,T5> &of )
   { return oneofTypeid<oneof<T1,T2,T3,T4,T5>,T>::type == of.type(); }
 
 template <typename T1, typename T2, typename T3, typename T4, typename T5>
@@ -232,7 +232,7 @@ class oneof {
       { _ONEOFDEBUG("oneof()"); }
     oneof(const this_type &x): valid(oneofTypeid<this_type,NILTYPE>::type) {
       if ( x.valid != oneofTypeid<this_type,NILTYPE>::type )
-        _ONEOFDEBUG("oneof(const oneof &) (T" << x.valid << ")");
+        _ONEOFDEBUG("oneof(const oneof &) (T" << static_cast<size_t>(x.valid) << ")");
       else
         _ONEOFDEBUG("oneof(const oneof &) ()");
       *this = x;
@@ -260,7 +260,7 @@ class oneof {
     
     this_type &operator = (const this_type &x) {
       if ( x.valid != oneofTypeid<this_type,NILTYPE>::type )
-        _ONEOFDEBUG("oneof = const oneof & (T" << x.valid << ")");
+        _ONEOFDEBUG("oneof = const oneof & (T" << static_cast<size_t>(x.valid) << ")");
       else
         _ONEOFDEBUG("oneof = const oneof & ()");
       switch( x.valid ) {
@@ -319,7 +319,7 @@ class oneof {
     
     void reset() {
       if ( valid != oneofTypeid<this_type,NILTYPE>::type )
-        _ONEOFDEBUG("oneof.reset() (T" << valid << ")");
+        _ONEOFDEBUG("oneof.reset() (T" << static_cast<size_t>(valid) << ")");
       else
         _ONEOFDEBUG("oneof.reset() ()");
       switch( valid ) {
