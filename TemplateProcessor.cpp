a5b59e05a56b40aca3d5e265549fa91ceef177e1
Joachim Falk <falk@cs.fau.de>
2008-07-01 16:24:04 +0000

    * Added CoSupport::String::TemplateProcessor formerly residing in Synthesis-SW
    git-archimport-id: falk@cs.fau.de--2008-common-intern/HscdSupport--mainline--0.8--patch-29

---
+++ TemplateProcessor.cpp
@@ -0,0 +1,115 @@
+
+#include <CoSupport/String/TemplateProcessor.hpp>
+
+#include <stdexcept>
+#include <sstream>
+#include <iostream>
+
+#include <errno.h>
+
+#include <boost/regex.hpp>
+
+namespace CoSupport { namespace String {
+
+namespace {
+
+  static const boost::regex reTemplateVariable("<@([[:alnum:]]+)@>");
+
+} // namespace Anonymous
+
+TemplateProcessor::TemplateProcessor(const std::string &templateFileName)
+  : templateFileName(templateFileName),
+    templateFile(templateFileName.c_str(), std::ios::in) {
+  if (!templateFile.is_open()) {
+    std::ostringstream msg;
+    msg << "Can't open template file '"
+        << templateFileName << "' :"
+        << strerror(errno);
+    throw std::runtime_error(msg.str());
+  }
+}
+
+void TemplateProcessor::fillIn(
+    const std::string &outputFileName,
+    const std::map<std::string, std::string> &fields) {
+  std::ofstream out(outputFileName.c_str(), std::ios::out);
+  if (!out.is_open()) {
+    std::ostringstream msg;
+    msg << "Can't open template output file '"
+        << outputFileName << "' :"
+        << strerror(errno);
+    throw std::runtime_error(msg.str());
+  }
+  
+  fillIn(out, fields);
+  
+  if (out.bad()) {
+    std::ostringstream msg;
+    msg << "Can't write to output file '"
+        << outputFileName << "' :"
+        << strerror(errno);
+    throw std::runtime_error(msg.str());
+  }
+}
+
+void TemplateProcessor::fillIn(
+    std::ostream &out,
+    const std::map<std::string, std::string> &fields) {
+  templateFile.clear();
+  templateFile.seekg(0, std::ios::beg);
+  
+  while (templateFile.good() && out.good()) {
+    std::string templateStringLine;
+    std::getline(templateFile, templateStringLine);
+    
+    std::string templateStringLineNew;
+    size_t      templateStringLinePos = 0;
+    
+    // iterate over all template variables in the line
+    for (boost::regex_iterator<std::string::const_iterator>
+           iter(
+             templateStringLine.begin(),
+             templateStringLine.end(),
+             reTemplateVariable);
+         iter !=  boost::regex_iterator<std::string::const_iterator>();
+         ++iter) {
+      // (*iter)[0] is the whole match string, e.g., "<@TEMPLATEVAR@>"
+      // (*iter)[1] is the name of the template variable, e.g., "TEMPLATEVAR"
+      
+      // Append stuff before template match
+      templateStringLineNew.append(
+        templateStringLine.begin() + templateStringLinePos,
+        templateStringLine.begin() + iter->position(0));
+      // Advance templateStringLinePos after "<@TEMPLATEVAR@>"
+      templateStringLinePos =
+        iter->position(0) + iter->length(0);
+      // Find template variable definition
+      std::map<std::string, std::string>::const_iterator viter =
+        fields.find((*iter)[1]);
+      if (viter != fields.end()) {
+        templateStringLineNew.append(viter->second);
+      } else {
+        templateStringLineNew
+          .append("!!!")
+          .append((*iter)[1])
+          .append(":UNDEF!!!");
+      }
+    }
+    // Append rest of template file line
+    templateStringLineNew.append(
+      templateStringLine.begin() + templateStringLinePos,
+      templateStringLine.end());
+    
+    out << templateStringLineNew << '\n';
+  }
+  if (templateFile.bad()) {
+    std::ostringstream msg;
+    msg << "Can't read from template file '"
+        << templateFileName << "' :"
+        << strerror(errno);
+    throw std::runtime_error(msg.str());
+  }
+}
+
+} } // namespace CoSupport::String
+
