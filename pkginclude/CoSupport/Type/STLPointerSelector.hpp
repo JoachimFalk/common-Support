f09d3c2a49538a2d6f48d662522f7be6500856d7
Joachim Falk <falk@cs.fau.de>
2008-12-02 16:36:14 +0000

    * Merge from HscdSupport--devel--0.8 * Change ListFacade to explicitly provide the types reference, const_reference, pointer and const_pointer. * No longer use CoSupport::Type::Const/Mutable to change between iterator and const_iterator now use - Type::STLIteratorSelector<C>::type to access const_iterator if container C is const and iterator otherwise. - Type::STLReverseIteratorSelector<C>::type to access const_reverse_iterator if container C is const and reverse_iterator otherwise. - Type::STLPointerSelector<C>::type to access const_pointer if container C is const and pointer otherwise. - Type::STLReferenceSelector<C>::type to access const_reference if container C is const and reference otherwise.
    Patches applied:
    
     * falk@cs.fau.de--2008-common-intern/HscdSupport--devel--0.8--patch-6
       
     * Change ListFacade to explicitly provide the types reference, const_reference, pointer and const_pointer.
     * Now longer use CoSupport::Type::Const/Mutable to change between iterator and const_iterator now use
     - Type::STLIteratorSelector<C>::type to access const_iterator if container C is const and iterator otherwise.
     - Type::STLReverseIteratorSelector<C>::type to access const_reverse_iterator if container C is const and reverse_iterator otherwise.
     - Type::STLPointerSelector<C>::type to access const_pointer if container C is const and pointer otherwise.
     - Type::STLReferenceSelector<C>::type to access const_reference if container C is const and reference otherwise.
    
    
    git-archimport-id: falk@cs.fau.de--2008-common-intern/HscdSupport--mainline--0.8--patch-71

---
+++ pkginclude/CoSupport/Type/STLPointerSelector.hpp
@@ -0,0 +1,69 @@
+// vim: set sw=2 ts=8:
+/*
+ * Copyright (c) 2004-2008 Hardware-Software-CoDesign, University of
+ * Erlangen-Nuremberg. All rights reserved.
+ * 
+ *   This library is free software; you can redistribute it and/or modify it under
+ *   the terms of the GNU Lesser General Public License as published by the Free
+ *   Software Foundation; either version 2 of the License, or (at your option) any
+ *   later version.
+ * 
+ *   This library is distributed in the hope that it will be useful, but WITHOUT
+ *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *   FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ *   details.
+ * 
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with this library; if not, write to the Free Software Foundation, Inc.,
+ *   59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ * 
+ * --- This software and any associated documentation is provided "as is" 
+ * 
+ * IN NO EVENT SHALL HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN NUREMBERG
+ * BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR
+ * CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS
+ * DOCUMENTATION, EVEN IF HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN
+ * NUREMBERG HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN NUREMBERG, SPECIFICALLY
+ * DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED
+ * HEREUNDER IS ON AN "AS IS" BASIS, AND HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF
+ * ERLANGEN NUREMBERG HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ * ENHANCEMENTS, OR MODIFICATIONS.
+ */
+
+#ifndef _INCLUDED_COSUPPORT_TYPE_STLPOINTERSELECTOR_HPP
+#define _INCLUDED_COSUPPORT_TYPE_STLPOINTERSELECTOR_HPP
+
+#include <boost/mpl/if.hpp>
+
+#include <boost/type_traits/is_const.hpp>
+#include <boost/type_traits/add_const.hpp>
+#include <boost/type_traits/remove_const.hpp>
+
+#include <boost/type_traits/is_reference.hpp>
+#include <boost/type_traits/remove_reference.hpp>
+#include <boost/type_traits/add_reference.hpp>
+
+#include <boost/type_traits/is_pointer.hpp>
+#include <boost/type_traits/remove_pointer.hpp>
+#include <boost/type_traits/add_pointer.hpp>
+
+namespace CoSupport { namespace Type {
+
+/// Map pointer to const_pointer if the container type is const itself.
+/// This behaviour is useful for generic programming if the implemented
+/// algorithm is independent of the constness of the provided STL container.
+template <typename Container>
+struct STLPointerSelector {
+  typedef typename boost::mpl::if_<
+      boost::is_const<Container>,
+      typename Container::const_pointer,
+      typename Container::pointer
+    >::type type;
+};
+
+} } // namespace CoSupport::Type
+
+#endif // _INCLUDED_COSUPPORT_TYPE_STLPOINTERSELECTOR_HPP
