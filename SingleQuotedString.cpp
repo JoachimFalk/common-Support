682e29a819fd424cb921ba613b9b591314d010ac
Joachim Falk <falk@cs.fau.de>
2009-06-09 15:33:19 +0000

    * Merge from HscdSupport--devel--0.8 branch - Added QuotedString stuff.
    Patches applied:
    
     * falk@cs.fau.de--2008-common-intern/HscdSupport--devel--0.8--patch-26
       
     * Added QuotedString stuff.
    
    
    git-archimport-id: falk@cs.fau.de--2008-common-intern/HscdSupport--mainline--0.8--patch-89

---
+++ SingleQuotedString.cpp
@@ -0,0 +1,83 @@
+// vim: set sw=2 ts=8:
+/*
+ * Copyright (c) 2004-2009 Hardware-Software-CoDesign, University of
+ * Erlangen-Nuremberg. All rights reserved.
+ * 
+ *   This library is free software; you can redistribute it and/or modify it under
+ *   the terms of the GNU Lesser General Public License as published by the Free
+ *   Software Foundation; either version 2 of the License, or (at your option) any
+ *   later version.
+ * 
+ *   This library is distributed in the hope that it will be useful, but WITHOUT
+ *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *   FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ *   details.
+ * 
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with this library; if not, write to the Free Software Foundation, Inc.,
+ *   59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ * 
+ * --- This software and any associated documentation is provided "as is" 
+ * 
+ * IN NO EVENT SHALL HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN NUREMBERG
+ * BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR
+ * CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS
+ * DOCUMENTATION, EVEN IF HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN
+ * NUREMBERG HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN NUREMBERG, SPECIFICALLY
+ * DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED
+ * HEREUNDER IS ON AN "AS IS" BASIS, AND HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF
+ * ERLANGEN NUREMBERG HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ * ENHANCEMENTS, OR MODIFICATIONS.
+ */
+
+#include <iostream>
+#include <string>
+
+#include <CoSupport/String/SingleQuotedString.hpp>
+
+namespace CoSupport { namespace String {
+
+std::istream &operator >>(std::istream &in, SingleQuotedString &dst) {
+  std::istream::sentry sentry(in, false);
+  
+  if (sentry) {
+    try {
+      int ch = in.get();
+      if (ch == '\'') {
+          dst.clear();
+          while ((ch = in.get()) != EOF && ch != '\'')
+            dst.append(1, ch);
+          if (ch != '\'')
+            in.setstate(std::ios_base::badbit);
+      } else {
+        if (ch != EOF)
+          in.unget();
+        in.setstate(std::ios_base::failbit);
+      }
+    } catch (...) {
+      in.setstate(std::ios_base::badbit);
+    }
+  }
+  return in;
+}
+
+std::ostream &operator <<(std::ostream &out, const SingleQuotedString &src) {
+  std::ostream::sentry sentry(out);
+  
+  if (sentry) {
+    try {
+      if (src.find('\'') == SingleQuotedString::npos)
+        out << '\'' << static_cast<const std::string &>(src) << '\'';
+      else
+        out.setstate(std::ios_base::failbit);
+    } catch (...) {
+      out.setstate(std::ios_base::badbit);
+    }
+  }
+  return out;
+}
+
+} } // namespace CoSupport::String
