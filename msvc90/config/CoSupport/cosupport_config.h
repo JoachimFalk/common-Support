778a5389ab566831f53f7710ef873617d906551e
Sebastian Graf <sebastian.graf@cs.fau.de>
2010-09-16 15:45:01 +0200

    Updated Visual-Studio-Solutions to latest version of Support

---
+++ msvc90/config/CoSupport/cosupport_config.h
@@ -0,0 +1,94 @@
+/* pkginclude/CoSupport/cosupport_config.h.  Generated from cosupport_config.h.in by configure.  */
+/* vim: set sw=2 ts=8 syn=c: */
+
+#ifndef _INCLUDED_COSUPPORT_CONFIG_H
+#define _INCLUDED_COSUPPORT_CONFIG_H
+
+/* Defined if debug support is enabled */
+#define COSUPPORT_ENABLE_DEBUG yes
+
+/* Defined if SystemC support is enabled */
+#define COSUPPORT_ENABLE_SYSTEMC yes
+
+/* Defined if xalan support is enabled */
+#define COSUPPORT_ENABLE_XALAN yes
+
+/* Defined if xerces support is enabled */
+#define COSUPPORT_ENABLE_XERCES yes
+
+/* Define to 1 if you have the long long type. */
+#define COSUPPORT_HAVE_LONG_LONG 1
+
+/* Define to 1 if you have the <stddef.h> header file. */
+#define COSUPPORT_HAVE_STDDEF_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#undef COSUPPORT_HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define COSUPPORT_HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define COSUPPORT_HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the size_t type. */
+#define COSUPPORT_HAVE_SIZE_T 1
+
+/* Define to the replacement type for size_t if this type is missing. */
+/* #undef COSUPPORT_REPLTYP_SIZE_T */
+
+/* Define to 1 if you have the ssize_t type. */
+#define COSUPPORT_HAVE_SSIZE_T 1
+
+/* Define to the replacement type for ssize_t if this type is missing. */
+/* #undef COSUPPORT_REPLTYP_SSIZE_T */
+
+/* Define to 1 if you have the int8_t type. */
+#undef COSUPPORT_HAVE_INT8_T 1
+
+/* Define to the replacement type for int8_t if this type is missing. */
+/* #undef COSUPPORT_REPLTYP_INT8_T */
+
+/* Define to 1 if you have the uint8_t type. */
+#undef COSUPPORT_HAVE_UINT8_T 1
+
+/* Define to the replacement type for uint8_t if this type is missing. */
+/* #undef COSUPPORT_REPLTYP_UINT8_T */
+
+/* Define to 1 if you have the int16_t type. */
+#undef COSUPPORT_HAVE_INT16_T 1
+
+/* Define to the replacement type for int16_t if this type is missing. */
+/* #undef COSUPPORT_REPLTYP_INT16_T */
+
+/* Define to 1 if you have the uint16_t type. */
+#undef COSUPPORT_HAVE_UINT16_T 1
+
+/* Define to the replacement type for uint16_t if this type is missing. */
+/* #undef COSUPPORT_REPLTYP_UINT16_T */
+
+/* Define to 1 if you have the int32_t type. */
+#undef COSUPPORT_HAVE_INT32_T 1
+
+/* Define to the replacement type for int32_t if this type is missing. */
+/* #undef COSUPPORT_REPLTYP_INT32_T */
+
+/* Define to 1 if you have the uint32_t type. */
+#undef COSUPPORT_HAVE_UINT32_T 1
+
+/* Define to the replacement type for uint32_t if this type is missing. */
+/* #undef COSUPPORT_REPLTYP_UINT32_T */
+
+/* Define to 1 if you have the int64_t type. */
+#undef COSUPPORT_HAVE_INT64_T 1
+
+/* Define to the replacement type for int64_t if this type is missing. */
+/* #undef COSUPPORT_REPLTYP_INT64_T */
+
+/* Define to 1 if you have the uint64_t type. */
+#undef COSUPPORT_HAVE_UINT64_T 1
+
+/* Define to the replacement type for uint64_t if this type is missing. */
+/* #undef COSUPPORT_REPLTYP_UINT64_T */
+
+#endif /* _INCLUDED_COSUPPORT_CONFIG_H */
