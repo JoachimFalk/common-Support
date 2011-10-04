6998088406c627c1d62c0dcfe4da0bfa40cb5d35
Joachim Falk <falk@cs.fau.de>
2011-10-04 13:55:26 +0200

    Added stl_output_for_(list|vector|set|map) tester

---
+++ test_stl_output_for_xxx.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2004-2009 Hardware-Software-CoDesign, University of
+ * Erlangen-Nuremberg. All rights reserved.
+ * 
+ *   This library is free software; you can redistribute it and/or modify it under
+ *   the terms of the GNU Lesser General Public License as published by the Free
+ *   Software Foundation; either version 2 of the License, or (at your option) any
+ *   later version.
+ * 
+ *   This library is distributed in the hope that it will be useful, but WITHOUT
+ *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *   FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ *   details.
+ * 
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with this library; if not, write to the Free Software Foundation, Inc.,
+ *   59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ * 
+ * --- This software and any associated documentation is provided "as is" 
+ * 
+ * IN NO EVENT SHALL HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN NUREMBERG
+ * BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR
+ * CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS
+ * DOCUMENTATION, EVEN IF HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN
+ * NUREMBERG HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN NUREMBERG, SPECIFICALLY
+ * DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED
+ * HEREUNDER IS ON AN "AS IS" BASIS, AND HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF
+ * ERLANGEN NUREMBERG HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ * ENHANCEMENTS, OR MODIFICATIONS.
+ */
+
+#include <CoSupport/Streams/stl_output_for_list.hpp>
+#include <CoSupport/Streams/stl_output_for_vector.hpp>
+#include <CoSupport/Streams/stl_output_for_set.hpp>
+#include <CoSupport/Streams/stl_output_for_map.hpp>
+
+#include <iostream>
+
+#include <list>
+#include <vector>
+#include <set>
+#include <map>
+
+#include <utility>
+
+int main(int argc, char *argv[]) {
+  std::list<int>              list;
+  std::vector<int>            vector;
+  std::set<int>               set;
+  std::map<int, std::string>  map;
+
+  list.push_back(1); list.push_back(2); std::cout << list << std::endl;
+  vector.push_back(1); vector.push_back(2); std::cout << vector << std::endl;
+  set.insert(1); set.insert(2); std::cout << set << std::endl;
+  map.insert(std::make_pair(1, "A")); map.insert(std::make_pair(2, "B")); std::cout << map << std::endl;
+  return 0;
+}
