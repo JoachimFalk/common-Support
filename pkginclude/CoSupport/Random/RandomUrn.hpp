f61a2c76467cb5e9a184c06a6f7f302edc4944ba
Joachim Falk <falk@cs.fau.de>
2013-08-30 19:41:03 +0200

    Bugfix for CoSupport::Random::RandomUrn

--- pkginclude/CoSupport/Random/RandomUrn.hpp
+++ pkginclude/CoSupport/Random/RandomUrn.hpp
@@ -48,12 +48,15 @@ namespace CoSupport { namespace Random {
 
 template <typename T>
 struct RandomUrn: public RandomGenerator<T> {
+  /// The urn vector contains pairs. Each pair consists of
+  /// a value and an associated weight, where pair.first is
+  /// the weight of the value pair.second.
   RandomUrn(std::vector<std::pair<size_t, T> > const &urn)
       : RandomGenerator<T>(create(urn)) {}
 private:
   static boost::function<T (void)> create(std::vector<std::pair<size_t, T> > const &urn) {
     std::map<size_t, T> urnMap;
-    size_t              urnEndWeight;
+    size_t              urnEndWeight = 0;
     for (typename std::vector<std::pair<size_t, T> >::const_iterator iter = urn.begin();
          iter != urn.end();
          ++iter) {
@@ -71,7 +74,32 @@ private:
 
 template <typename T>
 std::istream &operator >> (std::istream &in, RandomUrn<T> &v) {
-  assert(!"YUP!!!");
+  std::vector<std::pair<size_t, T> > urn;
+
+  do {
+    char   ch;
+    T      value;
+    size_t weight = 1;
+
+    in >> value;
+    if (in.fail())
+      return in;
+    if (in.peek() == 'x') {
+      in.get(ch); // skip 'x'
+      in >> weight;
+      if (in.fail())
+        return in;
+    }
+    urn.push_back(std::make_pair(weight, value));
+    if (in.peek() == ',') {
+      in.get(ch); // skip ','
+    } else {
+      if (in.eof() && !in.bad())
+        in.clear();
+      break;
+    }
+  } while (true);
+  v = RandomUrn<T>(urn);
   return in;
 }
 
