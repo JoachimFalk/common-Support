69452976be8fb459284fb9d52ef0096e1edf9641
Joachim Falk <falk@cs.fau.de>
2014-03-10 11:33:38 +0100

    Improved testbench for CoSupport::Math::ModuloGroup

--- test_modulo.cpp
+++ test_modulo.cpp
@@ -2,13 +2,14 @@
 #include <CoSupport/Math/ModuloGroup.hpp>
 
 template <typename T1, typename T2, typename T3>
-void testModuloGroup() {
-  T1 a1(2);
-  T2 a2(3);
-  T3 a3(-1);
+void testModuloGroup(T1 const &m1 = T1(), T2 const &m2 = T2(), T3 const &m3 = T3()) {
+  CoSupport::Math::ModuloGroup<T1> a1(2, m1);
+  CoSupport::Math::ModuloGroup<T2> a2(3, m2);
+  CoSupport::Math::ModuloGroup<T3> a3(-1, m3);
   assert(a1 == 2);
   assert(a2 == 3);
   assert(a3 == 4);
+  // + - constant test
   assert(a1 -  1 == 1);
   assert(a1 -  2 == 0);
   assert(a1 -  3 == 4);
@@ -25,6 +26,7 @@ void testModuloGroup() {
   assert(a1 - -2 == 4);
   assert(a1 - -3 == 0);
   assert(a1 - -4 == 1);
+  // between test
   assert(a1.between(a1, a1));
   assert(a1.between(a1, a2));
   assert(a1.between(a1, a3));
@@ -52,21 +54,116 @@ void testModuloGroup() {
   assert(!a2.between(a3, a1));
   assert(!a3.between(a1, a2));
   assert(a3.between(a2, a1));
+  // a1 ++ -- test
+  assert(a1++ == 2);
+  assert(a1 == 3);
+  assert(a1-- == 3);
+  assert(a1 == 2);
+  assert(++a1 == 3);
+  assert(a1 == 3);
+  assert(--a1 == 2);
+  assert(a1 == 2);
+  // a2 ++ -- test
+  assert(a2++ == 3);
+  assert(a2 == 4);
+  assert(a2-- == 4);
+  assert(a2 == 3);
+  assert(++a2 == 4);
+  assert(a2 == 4);
+  assert(--a2 == 3);
+  assert(a2 == 3);
+  // a3 ++ -- test
+  assert(a3++ == 4);
+  assert(a3 == 0);
+  assert(a3-- == 0);
+  assert(a3 == 4);
+  assert(++a3 == 0);
+  assert(a3 == 0);
+  assert(--a3 == 4);
+  assert(a3 == 4);
+  // + - test
+  assert(a1 + a2 == a3 - a3);
+  assert(a1 + a3 == a3 - a2);
+  assert(a2 + a3 == a1);
+  assert(a1 - a2 == a3);
+  assert(a1 - a3 == a2);
+  assert(a2 - a3 == a3);
+  // += test
+  // assert(a1 == 2);
+  // assert(a2 == 3);
+  // assert(a3 == 4);
+  assert(a1 += 3 == 0);
+  assert(a2 += 4 == 2);
+  assert(a3 += -1 == 3);
+  assert(a1 -= a3 == 2);
+  assert(a3 -= 4 == 4);
+  assert(a2 -= a3 == 3);
 }
 
 int main(int argc, char *argv[]) {
   // ModuloGroup tests
   testModuloGroup<
-    CoSupport::Math::ModuloGroup<CoSupport::Math::CTModulus<int, 5> >,
-    CoSupport::Math::ModuloGroup<CoSupport::Math::CTModulus<int, 5> >,
-    CoSupport::Math::ModuloGroup<CoSupport::Math::CTModulus<int, 5> > >();
+    CoSupport::Math::CTModulus<int, 5>,
+    CoSupport::Math::CTModulus<int, 5>,
+    CoSupport::Math::CTModulus<int, 5> >();
   testModuloGroup<
-    CoSupport::Math::ModuloGroup<CoSupport::Math::CTModulus<unsigned int, 5> >,
-    CoSupport::Math::ModuloGroup<CoSupport::Math::CTModulus<unsigned int, 5> >,
-    CoSupport::Math::ModuloGroup<CoSupport::Math::CTModulus<unsigned int, 5> > >();
+    CoSupport::Math::CTModulus<int, 5>,
+    CoSupport::Math::CTModulus<int, 5>,
+    CoSupport::Math::Modulus<int>
+  >(CoSupport::Math::CTModulus<int, 5>(),
+    CoSupport::Math::CTModulus<int, 5>(),
+    CoSupport::Math::Modulus<int>(5));
   testModuloGroup<
-    CoSupport::Math::ModuloGroup<CoSupport::Math::CTModulus<int, 5> >,
-    CoSupport::Math::ModuloGroup<CoSupport::Math::CTModulus<unsigned int, 5> >,
-    CoSupport::Math::ModuloGroup<CoSupport::Math::CTModulus<int, 5> > >();
+    CoSupport::Math::CTModulus<int, 5>,
+    CoSupport::Math::Modulus<int>,
+    CoSupport::Math::CTModulus<int, 5>
+  >(CoSupport::Math::CTModulus<int, 5>(),
+    CoSupport::Math::Modulus<int>(5),
+    CoSupport::Math::CTModulus<int, 5>());
+  testModuloGroup<
+    CoSupport::Math::Modulus<int>,
+    CoSupport::Math::CTModulus<int, 5>,
+    CoSupport::Math::CTModulus<int, 5>
+  >(CoSupport::Math::Modulus<int>(5),
+    CoSupport::Math::CTModulus<int, 5>(),
+    CoSupport::Math::CTModulus<int, 5>());
+
+  testModuloGroup<
+    CoSupport::Math::Modulus<int>,
+    CoSupport::Math::Modulus<int>,
+    CoSupport::Math::Modulus<int>
+  >(CoSupport::Math::Modulus<int>(5),
+    CoSupport::Math::Modulus<int>(5),
+    CoSupport::Math::Modulus<int>(5));
+  testModuloGroup<
+    CoSupport::Math::Modulus<int>,
+    CoSupport::Math::Modulus<int>,
+    CoSupport::Math::CTModulus<int, 5>
+  >(CoSupport::Math::Modulus<int>(5),
+    CoSupport::Math::Modulus<int>(5),
+    CoSupport::Math::CTModulus<int, 5>());
+  testModuloGroup<
+    CoSupport::Math::Modulus<int>,
+    CoSupport::Math::CTModulus<int, 5>,
+    CoSupport::Math::Modulus<int>
+  >(CoSupport::Math::Modulus<int>(5),
+    CoSupport::Math::CTModulus<int, 5>(),
+    CoSupport::Math::Modulus<int>(5));
+  testModuloGroup<
+    CoSupport::Math::CTModulus<int, 5>,
+    CoSupport::Math::Modulus<int>,
+    CoSupport::Math::Modulus<int>
+  >(CoSupport::Math::CTModulus<int, 5>(),
+    CoSupport::Math::Modulus<int>(5),
+    CoSupport::Math::Modulus<int>(5));
+
+  testModuloGroup<
+    CoSupport::Math::CTModulus<unsigned int, 5>,
+    CoSupport::Math::CTModulus<unsigned int, 5>,
+    CoSupport::Math::CTModulus<unsigned int, 5> >();
+  testModuloGroup<
+    CoSupport::Math::CTModulus<int, 5>,
+    CoSupport::Math::CTModulus<unsigned int, 5>,
+    CoSupport::Math::CTModulus<int, 5> >();
   return 0;
 }
