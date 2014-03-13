7c59abfb79fc214ad86ded0421fd8bf6174e80cd
Joachim Falk <falk@cs.fau.de>
2014-03-13 13:35:19 +0100

    Impproved test_po regression test

--- test_po.cpp
+++ test_po.cpp
@@ -34,6 +34,7 @@
 
 #include <iostream>
 #include <cassert>
+#include <stdint.h>
 
 #include <map>
 
@@ -46,138 +47,201 @@ typedef std::map<T1Vector, int>                                     T1Map;
 typedef std::map<T2Vector, int>                                     T2Map;
 
 int main(int argc, char *argv[]) {
-  T1Vector t1vector1;
-  t1vector1.push_back(1);
-  t1vector1.push_back(2);
-  std::cout << "t1vector1: " << t1vector1 << ", t1vector1.size(): " << t1vector1.size() << std::endl;
-  assert(t1vector1.size() == 2 && t1vector1[0] == 1 && t1vector1[1] == 2);
-  t1vector1.insert(t1vector1.begin(), 0);
-  std::cout << "t1vector1: " << t1vector1 << ", t1vector1.size(): " << t1vector1.size() << std::endl;
-  assert(t1vector1.size() == 3 && t1vector1[0] == 0 && t1vector1[1] == 1 && t1vector1[2] == 2);
-  t1vector1.insert(t1vector1.begin(), 33);
-  std::cout << "t1vector1: " << t1vector1 << ", t1vector1.size(): " << t1vector1.size() << std::endl;
-  assert(t1vector1.size() == 4 && t1vector1[0] ==33 && t1vector1[1] == 0 && t1vector1[2] == 1 && t1vector1[3] == 2);
-  t1vector1.pop_back();
-  std::cout << "t1vector1: " << t1vector1 << ", t1vector1.size(): " << t1vector1.size() << std::endl;
-  assert(t1vector1.size() == 3 && t1vector1[0] ==33 && t1vector1[1] == 0 && t1vector1[2] == 1);
-  t1vector1.erase(--t1vector1.end()-1);
-  std::cout << "t1vector1: " << t1vector1 << ", t1vector1.size(): " << t1vector1.size() << std::endl;
-  assert(t1vector1.size() == 2 && t1vector1[0] ==33 && t1vector1[1] == 1);
-  assert(t1vector1.front() == 33);
-  assert(t1vector1.back() == 1);
-  t1vector1.insert(++t1vector1.begin(), 2, 55);
-  std::cout << "t1vector1: " << t1vector1 << ", t1vector1.size(): " << t1vector1.size() << std::endl;
-  assert(t1vector1.size() == 4 && t1vector1[0] ==33 && t1vector1[1] == 55 && t1vector1[2] == 55 && t1vector1[3] == 1);
-  
-  T1Vector t1vector2(4);
-  t1vector2[0] = 1;
-  t1vector2[1] = 2;
-  t1vector2[2] = 3;
-  t1vector2[3] = 4;
+  // Testing vector interface of T1Vector
+  {
+    T1Vector t1vector1;
+    t1vector1.push_back(1);
+    t1vector1.push_back(2);
+    std::cout << "t1vector1: " << t1vector1 << ", t1vector1.size(): " << t1vector1.size() << std::endl;
+    assert(t1vector1.size() == 2 && t1vector1[0] ==  1 && t1vector1[1] == 2);
+    t1vector1.insert(t1vector1.begin(), 0);
+    std::cout << "t1vector1: " << t1vector1 << ", t1vector1.size(): " << t1vector1.size() << std::endl;
+    assert(t1vector1.size() == 3 && t1vector1[0] ==  0 && t1vector1[1] == 1 && t1vector1[2] == 2);
+    t1vector1.insert(t1vector1.begin(), 33);
+    std::cout << "t1vector1: " << t1vector1 << ", t1vector1.size(): " << t1vector1.size() << std::endl;
+    assert(t1vector1.size() == 4 && t1vector1[0] == 33 && t1vector1[1] == 0 && t1vector1[2] == 1 && t1vector1[3] == 2);
+    t1vector1.pop_back();
+    std::cout << "t1vector1: " << t1vector1 << ", t1vector1.size(): " << t1vector1.size() << std::endl;
+    assert(t1vector1.size() == 3 && t1vector1[0] == 33 && t1vector1[1] == 0 && t1vector1[2] == 1);
+    t1vector1.erase(--t1vector1.end()-1);
+    std::cout << "t1vector1: " << t1vector1 << ", t1vector1.size(): " << t1vector1.size() << std::endl;
+    assert(t1vector1.size() == 2 && t1vector1[0] == 33 && t1vector1[1] == 1);
+    assert(t1vector1.front() == 33);
+    assert(t1vector1.back() == 1);
+    t1vector1.insert(++t1vector1.begin(), 2, 55);
+    std::cout << "t1vector1: " << t1vector1 << ", t1vector1.size(): " << t1vector1.size() << std::endl;
+    assert(t1vector1.size() == 4 && t1vector1[0] ==33 && t1vector1[1] == 55 && t1vector1[2] == 55 && t1vector1[3] == 1);
+  }
+  // Testing mod and div of PO vectors
+  {
+    T1Vector t1vector1(4);
+    t1vector1[0] = 33;
+    t1vector1[1] = 55;
+    t1vector1[2] = 55;
+    t1vector1[3] = 1;
 
-  std::cout << "div(" << t1vector1 << ", " << t1vector2 << "): " << div(t1vector1, t1vector2) << std::endl;
-  assert(div(t1vector1, t1vector2) == 0);
-  std::cout << "mod(" << t1vector1 << ", " << t1vector2 << "): " << mod(t1vector1, t1vector2) << std::endl;
-  assert(mod(t1vector1, t1vector2) == t1vector1);
-  t1vector1.pop_back(); t1vector1.push_back(13);
-  std::cout << "div(" << t1vector1 << ", " << t1vector2 << "): " << div(t1vector1, t1vector2) << std::endl;
-  assert(div(t1vector1, t1vector2) == 3);
-  std::cout << "mod(" << t1vector1 << ", " << t1vector2 << "): " << mod(t1vector1, t1vector2) << std::endl;
-  assert(mod(t1vector1, t1vector2) == t1vector1 - 3*t1vector2);
+    T1Vector t1vector2(4);
+    t1vector2[0] = 1;
+    t1vector2[1] = 2;
+    t1vector2[2] = 3;
+    t1vector2[3] = 4;
 
-  std::cout << "supremum(" << t1vector1 << ", " << t1vector2 << "): " << supremum(t1vector1, t1vector2) << std::endl;
-  std::cout << "infimum(" << t1vector1 << ", " << t1vector2 << "): " << infimum(t1vector1, t1vector2) << std::endl;
+    std::cout << "div(" << t1vector1 << ", " << t1vector2 << "): " << div(t1vector1, t1vector2) << std::endl;
+    assert(div(t1vector1, t1vector2) == 0);
+    std::cout << "mod(" << t1vector1 << ", " << t1vector2 << "): " << mod(t1vector1, t1vector2) << std::endl;
+    assert(mod(t1vector1, t1vector2) == t1vector1);
+    t1vector1.pop_back(); t1vector1.push_back(13);
+    std::cout << "div(" << t1vector1 << ", " << t1vector2 << "): " << div(t1vector1, t1vector2) << std::endl;
+    assert(div(t1vector1, t1vector2) == 3);
+    std::cout << "mod(" << t1vector1 << ", " << t1vector2 << "): " << mod(t1vector1, t1vector2) << std::endl;
+    assert(mod(t1vector1, t1vector2) == t1vector1 - 3*t1vector2);
+  }
+  // Testing normal operations on PO vectors
+  {
+    T1Vector t1vector1(4);
+    t1vector1[0] = 33;
+    t1vector1[1] = 55;
+    t1vector1[2] = 55;
+    t1vector1[3] = 1;
 
-  std::cout << "supremum(" << t1vector1 << ", " << 19 << "): " << supremum(t1vector1, 19) << std::endl;
-  std::cout << "infimum(" << t1vector1 << ", " << 19 << "): " << infimum(t1vector1, 19) << std::endl;
+    T1Vector t1vector2(4);
+    t1vector2[0] = 1;
+    t1vector2[1] = 2;
+    t1vector2[2] = 3;
+    t1vector2[3] = 4;
 
-  std::cout << "supremum(" << 3 << ", " << t1vector2 << "): " << supremum(3, t1vector2) << std::endl;
-  std::cout << "infimum(" << 3 << ", " << t1vector2 << "): " << infimum(3, t1vector2) << std::endl;
+    std::cout << "supremum(" << t1vector1 << ", " << t1vector2 << "): " << supremum(t1vector1, t1vector2) << std::endl;
+    std::cout << "infimum(" << t1vector1 << ", " << t1vector2 << "): " << infimum(t1vector1, t1vector2) << std::endl;
 
-  T1Vector t1vector3 = t1vector1 + t1vector2;
-  T1Vector t1vector4 = t1vector3 - t1vector1;
+    std::cout << "supremum(" << t1vector1 << ", " << 19 << "): " << supremum(t1vector1, 19) << std::endl;
+    std::cout << "infimum(" << t1vector1 << ", " << 19 << "): " << infimum(t1vector1, 19) << std::endl;
   
-  assert(t1vector4 == t1vector2 && "t1vector4 == t1vector2");
-  t1vector3 -= t1vector1;
-  assert(t1vector3 == t1vector2 && "t1vector3 == t1vector2");
-  t1vector3 += 13;
+    std::cout << "supremum(" << 3 << ", " << t1vector2 << "): " << supremum(3, t1vector2) << std::endl;
+    std::cout << "infimum(" << 3 << ", " << t1vector2 << "): " << infimum(3, t1vector2) << std::endl;
   
-  assert(t1vector3 >= 13 && "t1vector3 >= 13");
-  assert(13 <= t1vector3 && "13 <= t1vector3");
-  assert(t1vector3 >  13 && "t1vector3 >  13");
-  assert(13 <  t1vector3 && "13 <  t1vector3");
-  
-  assert(t1vector1+t1vector1 == 2*t1vector1 && "t1vector1+t1vector1 = 2*t1vector1");
-  assert(t1vector1+t1vector1 == t1vector1*2 && "t1vector1+t1vector1 = t1vector1*2");
-  
-  std::vector<size_t> projIdx;
-  projIdx.push_back(2);
-  projIdx.push_back(1);
-  std::vector<size_t> flummy;
-  flummy.push_back(77);
-  flummy.push_back(13);
+    T1Vector t1vector3 = t1vector1 + t1vector2;
+    T1Vector t1vector4 = t1vector3 - t1vector1;
 
-  std::cout << "proj(" << t1vector1 << ", " << projIdx << "): " << proj(t1vector1, projIdx) << std::endl;
-  proj(t1vector1, projIdx) = flummy;
-  std::cout << "proj(" << t1vector1 << ", " << projIdx << "): " << proj(t1vector1, projIdx) << std::endl;
-  assert(t1vector1[2] == 77 && t1vector1[1] == 13);
-  proj(t1vector1, projIdx) = 0;
-  std::cout << "proj(" << t1vector1 << ", " << projIdx << "): " << proj(t1vector1, projIdx) << std::endl;
-  assert(t1vector1[2] == 0 && t1vector1[1] == 0);
+    assert(t1vector4 == t1vector2 && "t1vector4 == t1vector2");
+    t1vector3 -= t1vector1;
+    assert(t1vector3 == t1vector2 && "t1vector3 == t1vector2");
+    t1vector3 += 13;
 
-  t1vector2 = t1vector1;
-  t1vector2 *= 4;
-  assert(t1vector2/2 == t1vector1*2 && "t1vector2/2 == t1vector1*2");
-  t1vector2 /= 2;
-  assert(t1vector2 == t1vector1*2 && "t1vector2 == t1vector1*2");
-  
-  std::cout << t1vector1.size() << std::endl;
+    assert(t1vector3 >= 13 && "t1vector3 >= 13");
+    assert(13 <= t1vector3 && "13 <= t1vector3");
+    assert(t1vector3 >  13 && "t1vector3 >  13");
+    assert(13 <  t1vector3 && "13 <  t1vector3");
 
-  t1vector3 = -t1vector1;
-  assert(t1vector3 + t1vector1 == 0);
+    assert(t1vector1+t1vector1 == 2*t1vector1 && "t1vector1+t1vector1 = 2*t1vector1");
+    assert(t1vector1+t1vector1 == t1vector1*2 && "t1vector1+t1vector1 = t1vector1*2");
+
+    t1vector2 = t1vector1;
+    t1vector2 *= 4;
+    assert(t1vector2/2 == t1vector1*2 && "t1vector2/2 == t1vector1*2");
+    t1vector2 /= 2;
+    assert(t1vector2 == t1vector1*2 && "t1vector2 == t1vector1*2");
+
+    std::cout << t1vector1.size() << std::endl;
+
+    t1vector3 = -t1vector1;
+    assert(t1vector3 + t1vector1 == 0);
+  }
+  // Testing vector projections
+  {
+    std::vector<size_t> projIdx;
+    projIdx.push_back(2);
+    projIdx.push_back(1);
+    std::vector<size_t> flummy;
+    flummy.push_back(77);
+    flummy.push_back(13);
   
-  std::vector<int> idx;
-  idx.push_back(3);
-  idx.push_back(2);
-  idx.push_back(1);
-  idx.push_back(0);
+    T1Vector t1vector1(4);
+    t1vector1[0] = 24;
+    t1vector1[1] = 35;
+    t1vector1[2] = 11;
+    t1vector1[3] = -19;
+
+    std::cout << "proj(" << t1vector1 << ", " << projIdx << "): " << proj(t1vector1, projIdx) << std::endl;
+    proj(t1vector1, projIdx) = flummy;
+    std::cout << "proj(" << t1vector1 << ", " << projIdx << "): " << proj(t1vector1, projIdx) << std::endl;
+    assert(t1vector1[2] == 77 && t1vector1[1] == 13);
+    proj(t1vector1, projIdx) = 0;
+    std::cout << "proj(" << t1vector1 << ", " << projIdx << "): " << proj(t1vector1, projIdx) << std::endl;
+    assert(t1vector1[2] == 0 && t1vector1[1] == 0);
   
-  t1vector4 = proj(t1vector1, idx);
-  t1vector4 = proj(t1vector1, static_cast<std::vector<int> const &>(idx));
-  t1vector4 = proj(static_cast<T1Vector const &>(t1vector1), idx);
-  t1vector4 = proj(static_cast<T1Vector const &>(t1vector1),
-                   static_cast<std::vector<int> const &>(idx));
-  
-  assert(proj(proj(t1vector1, idx), idx) == t1vector1 && "proj(proj(t1vector1, idx), idx) == t1vector1");
-  
-  T1Map t1map;
-  t1map.insert(std::make_pair(t1vector1, 13));
-  for(T1Vector::iterator iter = t1vector1.begin();
-      iter != t1vector1.end();
-      ++iter) {
-    if (iter != t1vector1.begin())
-      std::cout << ", ";
-    std::cout << *iter;
-    int v = *iter;
-    iter = t1vector1.insert(t1vector1.erase(iter), 2*v);
+    std::vector<int> idx;
+    idx.push_back(3);
+    idx.push_back(2);
+    idx.push_back(1);
+    idx.push_back(0);
+
+    T2Vector t2vector1 = proj(t1vector1, idx);
+    t2vector1 = proj(t1vector1, static_cast<std::vector<int> const &>(idx));
+    t2vector1 = proj(static_cast<T1Vector const &>(t1vector1), idx);
+    t2vector1 = proj(static_cast<T1Vector const &>(t1vector1),
+                     static_cast<std::vector<int> const &>(idx));
+
+    assert(proj(proj(t1vector1, idx), idx) == t1vector1 && "proj(proj(t1vector1, idx), idx) == t1vector1");
+  }
+  {
+    T1Vector t1vector1;
+    for (int i = 0; i <= 7; ++i)
+      t1vector1.push_back((static_cast<uint64_t>(i+42)*2147483647) % 3571);
+    T1Map t1map;
+//  t1map.insert(std::make_pair(t1vector1, 13));
+    for(T1Vector::iterator iter = t1vector1.begin();
+        iter != t1vector1.end();
+        ++iter) {
+      if (iter != t1vector1.begin())
+        std::cout << ", ";
+      std::cout << *iter;
+      int v = *iter;
+      iter = t1vector1.insert(t1vector1.erase(iter), 2*v);
+      t1map.insert(std::make_pair(t1vector1, 13));
+      *iter = *iter / 3;
+    }
+    std::cout << std::endl;
+    for(T1Vector::const_iterator iter = t1vector1.begin();
+        iter != t1vector1.end();
+        ++iter) {
+      if (iter != t1vector1.begin())
+        std::cout << ", ";
+      std::cout << *iter;
+    }
+    std::cout << std::endl;
+    for (T1Map::const_iterator iter = t1map.begin();
+         iter != t1map.end();
+         ++iter) {
+      for (T1Map::const_iterator jter = t1map.begin();
+           jter != t1map.end();
+           ++jter) {
+        std::cout << iter->first << " <= " << jter->first << " " << (iter->first <= jter->first) << std::endl;
+        assert((iter->first <= jter->first) == (iter == jter));
+
+        std::cout << iter->first << " >= " << jter->first << " " << (iter->first >= jter->first) << std::endl;
+        assert((iter->first >= jter->first) == (iter == jter));
+
+        std::cout << iter->first << " <  " << jter->first << " " << (iter->first <  jter->first) << std::endl;
+        assert((iter->first <  jter->first) == false);
+
+        std::cout << iter->first << " >  " << jter->first << " " << (iter->first >  jter->first) << std::endl;
+        assert((iter->first >  jter->first) == false);
+
+        std::cout << iter->first << " == " << jter->first << " " << (iter->first == jter->first) << std::endl;
+        assert((iter->first == jter->first) == (iter == jter));
+
+        std::cout << iter->first << " != " << jter->first << " " << (iter->first != jter->first) << std::endl;
+        assert((iter->first != jter->first) == (iter != jter));
+      }
+    }
+    t1vector1.erase(t1vector1.begin(), t1vector1.end());
+    assert(t1vector1.empty());
     t1map.insert(std::make_pair(t1vector1, 13));
-  }
-  std::cout << std::endl;
-  for(T1Vector::const_iterator iter = t1vector1.begin();
-      iter != t1vector1.end();
-      ++iter) {
-    if (iter != t1vector1.begin())
-      std::cout << ", ";
-    std::cout << *iter;
-  }
-  std::cout << std::endl;
-  t1vector1.erase(t1vector1.begin(), t1vector1.end());
-  assert(t1vector1.empty());
-  t1map.insert(std::make_pair(t1vector1, 13));
-  for (T1Map::const_iterator iter = t1map.begin();
-       iter != t1map.end();
-       ++iter) {
-    std::cout << iter->first << std::endl;
+    for (T1Map::const_iterator iter = t1map.begin();
+         iter != t1map.end();
+         ++iter) {
+      std::cout << iter->first << std::endl;
+    }
   }
   return 0;
 }
