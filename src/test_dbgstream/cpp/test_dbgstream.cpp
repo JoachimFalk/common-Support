50e2edfe600e401603be30740401dd6402c54104
Joachim Falk <joachim.falk@fau.de>
2018-12-06 15:49:28 +0100

    Modified test_dbgstream to dump to std::cout.

--- src/test_dbgstream/cpp/test_dbgstream.cpp
+++ src/test_dbgstream/cpp/test_dbgstream.cpp
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 
 #include <iostream>
-#include <fstream>
 
 #include <CoSupport/compatibility-glue/nullptr.h>
 
@@ -11,18 +10,7 @@
 namespace CS = CoSupport::Streams;
 
 int main(int argc, char *argv[]) {
-  if (argc <= 1) {
-    std::cerr << "Usage: "
-      << (argv[0] != nullptr ? argv[0] : "???")
-      << " testfile.txt" << std::endl;
-    exit(-1);
-  }
-  
-  std::ofstream *foo = new std::ofstream(argv[1]);
-
-//*foo << "foo" << std::endl;
-  
-  CS::DebugOStream dbg(*foo);
+  CS::DebugOStream dbg(std::cout);
   dbg << CS::Color::Red() << "foo" << std::endl;
 
 //foo->close();
@@ -30,7 +18,7 @@ int main(int argc, char *argv[]) {
   
 //*foo << "bar" << std::endl;
 
-  dbg << CS::Color::Red() << "bar" << std::endl;
+  dbg << CS::Color::Green() << "bar" << std::endl;
 
 //delete foo;
 
