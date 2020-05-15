995b7b1eb396a33f779e881a5e90b3c39ccbc095
Joachim Falk <joachim.falk@fau.de>
2020-05-15 16:28:29 +0200

    Updated copyright with correct years and authors for test cases.

--- testcases/src/test_dbgstream/cpp/test_dbgstream.cpp
+++ testcases/src/test_dbgstream/cpp/test_dbgstream.cpp
@@ -1,3 +1,27 @@
+// -*- tab-width:8; indent-tabs-mode:nil; c-basic-offset:2; -*-
+// vim: set sw=2 ts=8 sts=2 et:
+/*
+ * Copyright (c)
+ *   2010 FAU -- Joachim Falk <joachim.falk@fau.de>
+ *   2011 FAU -- Joachim Falk <joachim.falk@fau.de>
+ *   2015 FAU -- Joachim Falk <joachim.falk@fau.de>
+ *   2018 FAU -- Joachim Falk <joachim.falk@fau.de>
+ *   2020 FAU -- Joachim Falk <joachim.falk@fau.de>
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ */
 
 #include <stdlib.h>
 
